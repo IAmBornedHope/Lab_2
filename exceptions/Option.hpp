6ef84c2ef8c3e9e594983ec7b4850f11ccce2457
IAmBornedHope <realbeloff@gmail.com>
2026-05-22 01:10:44 +0300

    option

---
+++ exceptions/Option.hpp
@@ -0,0 +1,44 @@
+#pragma once
+#include <cstddef>
+
+template<class T>
+class Option {
+private:
+    union  {
+        std::byte data_[sizeof(T)];
+        T value_;
+    } storage_;
+
+    bool has_value_;
+
+public:
+    Option() : has_value_{false} {}
+
+    Option(const T& value) : has_value_(true) {
+        new (storage_.data_) T(value);
+    }
+
+    ~Option() {
+        if (has_value_) {
+            storage_.value_.~T();
+        }
+    }
+
+    bool has_value() const {
+        return has_value_;
+    }
+
+    T& get_value() {
+        if (!has_value_) {
+            throw EmptyOptionException("Option пустое");
+        }
+        return storage_.value_;
+    }
+
+    const T& get_value() const {
+        if (!has_value()) {
+            throw EmptyOptionException("Option пустое");
+        }
+        return storage_.value_;
+    }
+};
